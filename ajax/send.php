9d9fe9d9652a7b8a55fdde0e77af24419cb7e618
Giacomo Galatone <galatone@exasys.it>
2014-10-17 23:11:33 +0200

    Module complete rewrite

--- ajax/send.php
+++ ajax/send.php
@@ -1,69 +1,119 @@
 <?php
-define( '_JEXEC', 1 );
-define( '_VALID_MOS', 1 );
-define( 'JPATH_BASE', realpath(dirname(__FILE__).'/../../..' ));
-define( 'DS', DIRECTORY_SEPARATOR );
-require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
-require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
 
-// Avoid email sending with file direct access
-if (empty($_REQUEST['subject']) OR  empty($_REQUEST['EMail']) OR empty($_REQUEST['Message']) ) {
-	die('0');
-}
+class ajaxMailSend {
 
-$config =& JFactory::getConfig("site");
+	public function __construct() 
+	{	
 
-$db = JFactory::getDbo();
-$query = $db->getQuery(true);
-
-$query->select(
-		$db->quoteName(array('params')))
-      	->from($db->quoteName('#__modules'))
-      	->where($db->quoteName('module') . ' = '. $db->quote('mod_circle_contact')
-   		);
-
-$db->setQuery($query,0,1);
-$rows = $db->loadObjectList();
-
-foreach ($rows as $row) {
-   $row->params;
-}
-
-$params = new JRegistry;
-$params->loadString($row->params, 'JSON');
-
-// Configuration Settings
-$address = $params->get("emailto");
-$e_subject = $_REQUEST['subject'];
-$fromMail = $config->get( 'mailfrom' );
-	
-if(!isset($_POST["nl_sub"])){
-
-	//Stripping tags so if even user writes tags like <div></div> or <a href=""></a>, these are just getting cleared.
-	//Making ready the content for e-mail by the way
-	$mailContent = '';
-	foreach($_POST as $key => $val){
-
-		$_POST[$key] = strip_tags($val);
-		$mailContent .= $key.' : '.$_POST[$key].'<br>';
+		if($this->checkRequest()) {
+			$this->returnStatus(false);
+		} else {
+			$this -> getSiteConfig();
+			$config 	= 	$this -> getConfig();
+			$db 		= 	$this -> getDB();
+			$emailfrom 	= 	$this -> getEmailFrom($config);
+			$emailto 	= 	$this -> getEmailTo($db);
+			$headers 	= 	$this -> getHeaders($emailfrom);
+			$subject  	= 	$this -> getSubject();
+			$content  	= 	$this -> getContent();
+			$this-> sendEmail($emailto, $headers, $subject, $content);
+		}
 	}
 
-	//Getting rid of backslashes
-	$mailContent = $mailContent != "" ? stripslashes($mailContent) : '';
+	private function getSiteConfig() {
+		define( '_JEXEC', 1 );
+		define( '_VALID_MOS', 1 );
+		define( 'JPATH_BASE', realpath(dirname(__FILE__).'/../../..' ));
+		define( 'DS', DIRECTORY_SEPARATOR );
+		require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
+		require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
+	}
 
-	$headers  = "MIME-Version: 1.0 \n";
-	$headers .= "Content-type: text/html; charset=utf-8 \n";
-	$headers .= "From: "     . $fromMail. " \n".
+	private function getConfig() {
+		return JFactory::getConfig("site");
+	}
+
+	private function getDB() {
+		return JFactory::getDbo();
+	}
+
+	/* Avoid email sending with file direct access */
+	private function checkRequest() 
+	{
+		if (empty($_REQUEST['subject']) OR  empty($_REQUEST['EMail']) OR empty($_REQUEST['Message']) ) {
+			return true;
+		}
+	}
+
+	/* 
+	* Return the destination email address taken from the module parameter.
+	* The database connection is needed instead of JModuleHelper::getModule(),
+	* in fact it could not work if the module not assigned to all pages.
+	*/
+	private function getEmailTo($db) {
+		$query = $db->getQuery(true);
+
+		$query->select
+		(
+			$db->quoteName(array('params')))
+		  	->from($db->quoteName('#__modules'))
+		   	->where($db->quoteName('module') . ' = '. $db->quote('mod_circle_contact')
+		);
+
+		$db->setQuery($query,0,1);
+		$rows = $db->loadObjectList();
+
+		foreach ($rows as $row) {
+		   $row->params;
+		}
+
+		$params = new JRegistry;
+		$params->loadString($row->params, 'JSON');
+		return $params->get("emailto");
+	}
+
+	/*
+	* Get the sender address from the site configuration
+	*/
+	private function getEmailFrom($config) {
+		return $config->get( 'mailfrom' );
+	}
+
+	private function getHeaders($fromMail) {
+		$headers  = "MIME-Version: 1.0 \n";
+		$headers .= "Content-type: text/html; charset=utf-8 \n";
+		$headers .= "From: "     . $fromMail. " \n".
 		"Reply-To: " . $fromMail. " \n".
 		"X-Mailer: PHP/" . phpversion();
+		return $headers;
+	}
 
+	private function getSubject() {
+		return stripslashes(strip_tags($_REQUEST['subject']));
+	}
 
+	private function getContent() {
+		$mailContent = '';
+		foreach($_POST as $key => $val){
+				$_POST[$key] = strip_tags($val);
+				$mailContent .= $key.' : '.$_POST[$key].'<br>';
+		}
+		$mailContent = $mailContent != "" ? stripslashes($mailContent) : '';
+		return $mailContent;
+	}
 
-	if(mail($address, $e_subject, $mailContent, $headers)){
-		echo '1';
-	}else{
-		echo '0';
+	private function sendEmail($to, $header, $subject, $content) {
+		if(mail($to, $subject, $content, $header)){
+			$this->returnStatus(true);
+		}else{
+			$this->returnStatus(false);
+		}
+	}
+
+	private function returnStatus($value) {
+		if ($value == true) { echo "1"; } else { die(0); };
 	}
 }
 
+new ajaxMailSend();
 ?>
